2a8086b61d141b73eaf43b73e656970f1a62c4c6
Greiberth Perez <grei6095@gmail.com>
2023-11-24 11:42:21 +0000

    edited Prediction output
    Added "target":{} property

--- src/model_prediction.py
+++ src/model_prediction.py
@@ -5,13 +5,13 @@ from sklearn.tree import DecisionTreeClassifier
 from joblib import load
 
 
-
-def load_data(file_path)-> pd.DataFrame:
+def load_data(file_path) -> pd.DataFrame:
     """Load test data from CSV file"""
     # TODO: Load test data from CSV file
     df = pd.read_csv(file_path)
     df = df.drop(columns=['y'])
     return df
+
 
 def load_model(model_path) -> DecisionTreeClassifier:
     """Load trained model"""
@@ -19,14 +19,20 @@ def load_model(model_path) -> DecisionTreeClassifier:
     model = load(model_path)
     return model
 
+
 def make_predictions(df, model: DecisionTreeClassifier) -> list[dict[str, int]]:
     """Make predictions on test data"""
     # TODO: Use the model to make predictions on the test data
     predictions = model.predict(df)
-    
-    predictions_dict = {f"{i}": int(p) for i, p in enumerate(predictions)}
-    
+
+    predictions_dict = {
+        "target": {
+            f"{i}": int(p) for i, p in enumerate(predictions)
+        }
+    }
+
     return predictions_dict
+
 
 def save_predictions(predictions, predictions_file):
     """Save predictions to a JSON file"""
@@ -35,33 +41,37 @@ def save_predictions(predictions, predictions_file):
         json.dump(predictions, f)
     return
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Prediction script for Energy Forecasting Hackathon')
+    parser = argparse.ArgumentParser(
+        description='Prediction script for Energy Forecasting Hackathon')
     parser.add_argument(
-        '--input_file', 
-        type=str, 
-        default='data/processed_data_test.csv', 
+        '--input_file',
+        type=str,
+        default='data/processed_data_test.csv',
         help='Path to the test data file to make predictions'
     )
     parser.add_argument(
-        '--model_file', 
-        type=str, 
+        '--model_file',
+        type=str,
         default='models/model.pkl',
         help='Path to the trained model file'
     )
     parser.add_argument(
-        '--output_file', 
-        type=str, 
-        default='./predictions.json', 
+        '--output_file',
+        type=str,
+        default='./predictions.json',
         help='Path to save the predictions'
     )
     return parser.parse_args()
+
 
 def main(input_file, model_file, output_file):
     df = load_data(input_file)
     model = load_model(model_file)
     predictions = make_predictions(df, model)
     save_predictions(predictions, output_file)
+
 
 if __name__ == "__main__":
     args = parse_arguments()
